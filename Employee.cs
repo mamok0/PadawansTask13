598940d8409fac0dba4b0de69de6ee363ec0a9fa
Artem Kivachuk <mamokosf@gmail.com>
2019-02-14 23:33:21 +0300

    first solving

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string Surname;
+        private int Age;
+
+        public Employee()
+                {
+                    Surname = "";
+                    Age = 0;
+                }
+
+        public Employee(string surname, int age)
+                {
+                    Surname = (surname == null ? "" : surname);
+                    Age = age;
+                }
+
+        private string AgeToString()
+        {
+            return Age.ToString();
+        }
+
+        public void SetSurname(string newSurname)
+        {
+            this.Surname = newSurname;
+        }
+        
+        public string Get_Employee_Info()
+        {
+            return $"Surname: {this.Surname}, Age: {this.Age}";
+        }
+    }
+}
